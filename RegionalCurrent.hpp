a76b82d32b5676905d733640ff5d025cb8a668f4
karthikeyan chockalingam <karthikeyan.chockalingam@stfc.ac.uk>
2025-09-17 11:45:55 +0100

    Add RegionalCurrent

---
+++ RegionalCurrent.hpp
@@ -0,0 +1,36 @@
+#include "mfem.hpp"
+using namespace mfem;
+
+class RegionalCurrent
+{
+   private:
+      ParFiniteElementSpace * &_x_fespace;
+      OverPotentailCoefficient * _op;
+      ExchangeCurrentCoefficient * _jex;
+      const Region &_r;
+      TransformedCoefficient _i_tc;
+      PWCoefficient _i_pwc;
+
+   public:
+      /// P2D
+      RegionalCurrent(
+      ParFiniteElementSpace * &x_fespace,
+      const real_t & a,
+      OverPotentailCoefficient & op,
+      ExchangeCurrentCoefficient & jex
+      Region & r):
+      _x_fespace(x_fespace),
+      _op(&op),        
+      _jex(&jex),
+      _r(r),
+      _i_tc(_op, _jex, [=](real_t op, real_t jex) { return a * jex * exp( -0.5 * op); }),
+      {}
+
+      /// P2D
+      real_t operator() 
+      {
+        QuadratureSpace x_qspace(_x_fespace->GetParMesh(), 2 * _x_fespace->FEColl()->GetOrder());
+        _i_pwc.UpdateCoefficient(_r, _i_tc);
+        return x_qspace.Integrate(_i_pwc);
+      }
+};
